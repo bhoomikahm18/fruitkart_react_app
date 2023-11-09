c8575abe3254d6bce2da79f1077d160605920c13
Bhoomika <bhoomikahm18@gmail.com>
2023-11-09 13:19:06 +0530

    Adding header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Products from './components/products/Products.jsx';
 import Signup from './components/signup/Signup.jsx';
 import Cart from './components/cart/Cart.jsx';
-// import { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/header/Header.jsx';
 
 function App() {
 
-  // const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   return (
     <div>
       <BrowserRouter>
-      <Header cartItems={null}/>
+      <Header cartItems={cartItems}/>
       <Routes>
         <Route exact path="/" element={<Products />}/>
         <Route exact path="/signup" element={<Signup />}/>
-        <Route exact path="/cart" element={<Cart />}/>
+        <Route exact path="/cart" element={<Cart cartItems={cartItems}/>}/>
       </Routes>
       </BrowserRouter>
     </div>
