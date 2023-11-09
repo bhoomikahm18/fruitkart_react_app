cfbe183ead30516dbd3654b26d9a704af3378f3d
Bhoomika <bhoomikahm18@gmail.com>
2023-11-09 12:43:24 +0530

    Function to display content on page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Products from './components/products/Products.jsx';
+import Signup from './components/signup/Signup.jsx';
+import Cart from './components/cart/Cart.jsx';
+// import { useState } from 'react';
+import Header from './components/header/Header.jsx';
 
 function App() {
+
+  // const [cartItems, setCartItems] = useState([]);
+
   return (
     <div>
-      hello
+      <BrowserRouter>
+      <Header cartItems={null}/>
+      <Routes>
+        <Route exact path="/" element={<Products />}/>
+        <Route exact path="/signup" element={<Signup />}/>
+        <Route exact path="/cart" element={<Cart />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
