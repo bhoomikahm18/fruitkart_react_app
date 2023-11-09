97bc7cb61d67c744544424f6035306a5051a5574
Bhoomika <bhoomikahm18@gmail.com>
2023-11-09 13:53:40 +0530

    Styling products display

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './Products.css';
 
 function Products(props) {
   const [products, setProducts] = useState({ flag: false, productsList: null });
